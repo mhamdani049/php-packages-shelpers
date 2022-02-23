9c2845397756a0ee8f90a4bd9624caee3e7adee2
Muhamad Yusup Hamdani <myhamdani1014@pnm.co.id>
2022-02-23 15:41:00 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '47fac33772c1f6e17fe9d4a06230c66052cd71cf',
+        'name' => 'myhamdani/shelpers',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myhamdani/shelpers' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '47fac33772c1f6e17fe9d4a06230c66052cd71cf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
