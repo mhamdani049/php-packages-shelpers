6dcf3ad0995baebaf791acda05ca8806f22c0cf9
Muhamad Yusup Hamdani <32784378+mhamdani049@users.noreply.github.com>
2023-01-10 13:39:48 +0700

    Update ResponseShelpers.php

--- src/ResponseShelpers.php
+++ src/ResponseShelpers.php
@@ -1,23 +1,52 @@
 <?php
 namespace Myhamdani\Shelpers;
 
+use App\Services\AuditService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
 
-class ResponseShelpers {
-    public static function generate($data = [], $message = "", $code = "00", $status = "success", $metadata = null): JsonResponse
-    {
-        $responseCode = ($status == "success") ? 200 : 400;
-        if ($status == "unauthorized") $responseCode = 401;
+class ResponseShelpers
+{
+    public static function generate(
+        $data = [],
+        $message = '',
+        $code = '00',
+        $status = 'success',
+        $metadata = null,
+        $errorDetail = null
+    ): JsonResponse {
+        $auditService = new AuditService();
+
+        $responseCode = $status == 'success' ? 200 : 400;
+        if ($status == 'unauthorized') {
+            $responseCode = 401;
+        }
         $responseData = [
             'STATUS' => $status,
             'CODE' => (string) $code,
             'MESSAGE' => $message,
-            'DATA' => $data
+            'DATA' => $data,
         ];
-        if ($metadata) $responseData["METADATA"] = $metadata;
+        if ($metadata) {
+            $responseData['METADATA'] = $metadata;
+        }
 
+        if ($status == 'error' && $errorDetail != null) {
+            $responseData['DATA'] = null;
+            $responseData['ERRORS'] = $errorDetail;
+        }
+
+        if ($status == 'error' && $errorDetail == null) {
+            $responseData['ERRORS'] = [
+                'TYPE' => 'Stacktrace',
+                'DATA' => null,
+            ];
+        }
+
+        if (!in_array($responseCode, [401, 500, 503])) {
+            $auditService->record(request(), $responseData);
+        }
         return response()->json($responseData, $responseCode);
     }
 
@@ -28,17 +57,23 @@ class ResponseShelpers {
         if (count($rawResult) > 1) {
             $response = $rawResult;
 
-            Log::info("SP : " . $exec . ' - Params: ' . json_encode($paramsExec) . ' - Response: success - ' . json_encode($response));
+            Log::info(
+                'SP : ' .
+                    $exec .
+                    ' - Params: ' .
+                    json_encode($paramsExec) .
+                    ' - Response: success - ' .
+                    json_encode($response)
+            );
             return $response;
         }
 
-        foreach($rawResult as $x) {
+        foreach ($rawResult as $x) {
             if (is_object($x)) {
                 if (isset($x->STATUS) || isset($x->USERNAME)) {
                     $response = $x;
-                }
-                else {
-                    foreach($x as $b) {
+                } else {
+                    foreach ($x as $b) {
                         try {
                             $response = json_decode($b)[0];
                         } catch (\Throwable $th) {
@@ -52,18 +87,39 @@ class ResponseShelpers {
         }
 
         if (!$response) {
-            $responseJson = response()->json([
-                'STATUS' => 'FALSE',
-                'CODE' => '10',
-                'MESSAGE' => Lang::get("global.somethingWentWrong"),
-                'DATA' => null
-            ], 400);
+            $responseJson = response()->json(
+                [
+                    'STATUS' => 'FALSE',
+                    'CODE' => '10',
+                    'MESSAGE' => Lang::get('global.somethingWentWrong'),
+                    'DATA' => null,
+                    'ERRORS' => [
+                        'TYPE' => 'Exception',
+                        'DATA' => null,
+                    ],
+                ],
+                400
+            );
 
-            Log::info("SP : " . $exec . ' - Params: ' . json_encode($paramsExec) . ' - Response: error - ' . json_encode($responseJson));
+            Log::info(
+                'SP : ' .
+                    $exec .
+                    ' - Params: ' .
+                    json_encode($paramsExec) .
+                    ' - Response: error - ' .
+                    json_encode($responseJson)
+            );
             return $responseJson;
         }
 
-        Log::info("SP : " . $exec . ' - Params: ' . json_encode($paramsExec) . ' - Response: success - ' . json_encode($response));
+        Log::info(
+            'SP : ' .
+                $exec .
+                ' - Params: ' .
+                json_encode($paramsExec) .
+                ' - Response: success - ' .
+                json_encode($response)
+        );
         return $response;
     }
 }
